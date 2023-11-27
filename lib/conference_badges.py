7650f7755a0f35d84f93c93240c75e75c757783e
VERA-OBIERO <vera.obiero@gmail.com>
2023-11-27 19:33:01 +0300

    done

--- lib/conference_badges.py
+++ lib/conference_badges.py
@@ -1,11 +1,26 @@
 def badge_maker(name):
-    return None
+    message = "Hello, my name is " + name + "."
+    return message
 
 def batch_badge_creator(names):
-    return None
+    badges = []
+    for name in names:
+        badges.append(badge_maker(name))
+    return badges
 
 def assign_rooms(names):
-    return None
+    rooms = []
+    index = 0
+    for name in names:
+        rooms.append(f"Hello, {name}! You'll be assigned to room {index + 1}!")
+        index += 1
+    return rooms
 
 def printer(names):
-    return None
+    badge_list = batch_badge_creator(names)
+    rooms_list = assign_rooms(names)
+    for badge in badge_list:
+        print(badge)
+    for room in rooms_list:
+        print(room)
+
